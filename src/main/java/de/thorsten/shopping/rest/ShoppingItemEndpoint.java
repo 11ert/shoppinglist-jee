03f76bda279c5737023db62187126ef4c99616de
Thorsten Elfert <thorsten.elfert@gmail.com>
2015-11-15 19:56:45 +0100

    nach forge

--- src/main/java/de/thorsten/shopping/rest/ShoppingItemEndpoint.java
+++ src/main/java/de/thorsten/shopping/rest/ShoppingItemEndpoint.java
@@ -5,18 +5,9 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
-import javax.persistence.OptimisticLockException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
@@ -27,92 +18,69 @@ import de.thorsten.shopping.model.ShoppingItem;
  */
 @Stateless
 @Path("/shoppingitems")
-public class ShoppingItemEndpoint {
-	@PersistenceContext(unitName = "shopping-persistence-unit")
-	private EntityManager em;
+public class ShoppingItemEndpoint
+{
+   @PersistenceContext(unitName = "shopping-persistence-unit")
+   private EntityManager em;
 
-	@POST
-	@Consumes("application/json")
-	public Response create(ShoppingItem entity) {
-		em.persist(entity);
-		return Response.created(
-				UriBuilder.fromResource(ShoppingItemEndpoint.class)
-						.path(String.valueOf(entity.getId())).build()).build();
-	}
+   @POST
+   @Consumes("application/xml")
+   public Response create(ShoppingItem entity)
+   {
+      em.persist(entity);
+      return Response.created(UriBuilder.fromResource(ShoppingItemEndpoint.class).path(String.valueOf(entity.getId())).build()).build();
+   }
 
-	@DELETE
-	@Path("/{id:[0-9][0-9]*}")
-	public Response deleteById(@PathParam("id") Long id) {
-		ShoppingItem entity = em.find(ShoppingItem.class, id);
-		if (entity == null) {
-			return Response.status(Status.NOT_FOUND).build();
-		}
-		em.remove(entity);
-		return Response.noContent().build();
-	}
+   @DELETE
+   @Path("/{id:[0-9][0-9]*}")
+   public Response deleteById(@PathParam("id") Long id)
+   {
+      ShoppingItem entity = em.find(ShoppingItem.class, id);
+      if (entity == null)
+      {
+         return Response.status(Status.NOT_FOUND).build();
+      }
+      em.remove(entity);
+      return Response.noContent().build();
+   }
 
-	@GET
-	@Path("/{id:[0-9][0-9]*}")
-	@Produces("application/json")
-	public Response findById(@PathParam("id") Long id) {
-		TypedQuery<ShoppingItem> findByIdQuery = em
-				.createQuery(
-						"SELECT DISTINCT s FROM ShoppingItem s WHERE s.id = :entityId ORDER BY s.id",
-						ShoppingItem.class);
-		findByIdQuery.setParameter("entityId", id);
-		ShoppingItem entity;
-		try {
-			entity = findByIdQuery.getSingleResult();
-		} catch (NoResultException nre) {
-			entity = null;
-		}
-		if (entity == null) {
-			return Response.status(Status.NOT_FOUND).build();
-		}
-		return Response.ok(entity).build();
-	}
+   @GET
+   @Path("/{id:[0-9][0-9]*}")
+   @Produces("application/xml")
+   public Response findById(@PathParam("id") Long id)
+   {
+      TypedQuery<ShoppingItem> findByIdQuery = em.createQuery("SELECT DISTINCT s FROM ShoppingItem s WHERE s.id = :entityId ORDER BY s.id", ShoppingItem.class);
+      findByIdQuery.setParameter("entityId", id);
+      ShoppingItem entity;
+      try
+      {
+         entity = findByIdQuery.getSingleResult();
+      }
+      catch (NoResultException nre)
+      {
+         entity = null;
+      }
+      if (entity == null)
+      {
+         return Response.status(Status.NOT_FOUND).build();
+      }
+      return Response.ok(entity).build();
+   }
 
-	@GET
-	@Produces("application/json")
-	public List<ShoppingItem> listAll(
-			@QueryParam("start") Integer startPosition,
-			@QueryParam("max") Integer maxResult) {
-		TypedQuery<ShoppingItem> findAllQuery = em.createQuery(
-				"SELECT DISTINCT s FROM ShoppingItem s ORDER BY s.id",
-				ShoppingItem.class);
-		if (startPosition != null) {
-			findAllQuery.setFirstResult(startPosition);
-		}
-		if (maxResult != null) {
-			findAllQuery.setMaxResults(maxResult);
-		}
-		final List<ShoppingItem> results = findAllQuery.getResultList();
-		return results;
-	}
+   @GET
+   @Produces("application/xml")
+   public List<ShoppingItem> listAll()
+   {
+      final List<ShoppingItem> results = em.createQuery("SELECT DISTINCT s FROM ShoppingItem s ORDER BY s.id", ShoppingItem.class).getResultList();
+      return results;
+   }
 
-	@PUT
-	@Path("/{id:[0-9][0-9]*}")
-	@Consumes("application/json")
-	public Response update(@PathParam("id") Long id, ShoppingItem entity) {
-		if (entity == null) {
-			return Response.status(Status.BAD_REQUEST).build();
-		}
-		if (id == null) {
-			return Response.status(Status.BAD_REQUEST).build();
-		}
-		if (!id.equals(entity.getId())) {
-			return Response.status(Status.CONFLICT).entity(entity).build();
-		}
-		if (em.find(ShoppingItem.class, id) == null) {
-			return Response.status(Status.NOT_FOUND).build();
-		}
-		try {
-			entity = em.merge(entity);
-		} catch (OptimisticLockException e) {
-			return Response.status(Response.Status.CONFLICT)
-					.entity(e.getEntity()).build();
-		}
-
-		return Response.noContent().build();
-	}
+   @PUT
+   @Path("/{id:[0-9][0-9]*}")
+   @Consumes("application/xml")
+   public Response update(ShoppingItem entity)
+   {
+      entity = em.merge(entity);
+      return Response.noContent().build();
+   }
 }
